796454ca8b7b2351d21b8c8b43ec6f7e903e72af
centipedejam <jheimburge@gmail.com>
2023-08-18 13:50:16 -0700

    Add buildUnshuffledDeck and shuffleNewDeck functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 /*----- constants -----*/
-
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A',];
+const unshuffledDeck = buildUnshuffledDeck();
 
 /*----- state variables -----*/
 let shuffledDeck;
@@ -22,4 +24,26 @@ function init() {
 
 function render() {
 
+}
+
+function buildUnshuffledDeck() {
+    const deck = [];
+    suits.forEach(function (suit) {
+        ranks.forEach(function (rank) {
+            deck.push({
+                face: `${suit}${rank}`,
+                value: Number(rank) || (rank === 'A' ? 11 : 10)
+            });
+        });
+    });
+    return deck;
+}
+function shuffleNewDeck() {
+    const tempDeck = [...unshuffledDeck];
+    const newShuffledDeck = [];
+    while (tempDeck.length) {
+        const rndIdx = Math.floor(Math.random() * tempDeck.length);
+        newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+    }
+    return newShuffledDeck;
 }
